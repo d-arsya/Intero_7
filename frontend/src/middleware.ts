d37b872e548e681be552e2b8a8cc5b0705f84cad
emiliojuni0r <emiliohamsyahjr@gmail.com>
2025-06-01 02:15:24 +0700

    update middleware for yayasna n mitra

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -116,5 +116,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/donasi','/riwayat']
+  matcher: ['/donasi','/riwayat','/yayasan','/mitra']
 }
