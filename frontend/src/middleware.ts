3a4ba43e859b1667b20562778015f7329778db37
emiliojuni0r <emiliohamsyahjr@gmail.com>
2025-06-01 01:25:51 +0700

    update middleware utk riwayat

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -116,5 +116,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/donasi',
+  matcher: ['/donasi','/riwayat']
 }
